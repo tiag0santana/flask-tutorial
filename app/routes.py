4e280064730fd839c837fad1a3553d705f30840b
Tiago Santana <tiagosantana@outlook.com>
2025-01-29 15:16:54 -0300

    feat: adding templates for base and index

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Tiago'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ryu'},
+            'body': "Believe in the path you've chosen."
+        }
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
